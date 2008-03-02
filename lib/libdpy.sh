7a1a6dced39f29495d9d9d59f2b76d81ce59c8ff
adam <adam>
2008-03-02 22:40:25 +0000

    ffs

--- lib/libdpy.sh
+++ lib/libdpy.sh
@@ -8,8 +8,18 @@ dpy_geometry=( $(
 # this doesn't work under nomachine's NX client
 #dpy_geometry=($( xprop -root -notype 32i ' $0 $1\n' _NET_DESKTOP_GEOMETRY ))
 
-dpy_width=${dpy_geometry[1]}
-dpy_height=${dpy_geometry[2]}
+# bash counts from 0, zsh counts from 1 - ugh!
+_array_index_test=(0 1 2)
+if [[ "${_array_index_test[1]}" == 1 ]]; then
+    echo "shell array indices start at 0"
+    dpy_width=${dpy_geometry[0]}
+    dpy_height=${dpy_geometry[1]}
+elif [[ "${_array_index_test[1]}" == 0 ]]; then
+    echo "shell array indices start at 1"
+    dpy_width=${dpy_geometry[1]}
+    dpy_height=${dpy_geometry[2]}
+fi
+unset _array_index_test
 
 if echo "$dpy_width$dpy_height" | egrep -q '[^0-9]'; then
     echo "xprop failed to determine _NET_DESKTOP_GEOMETRY; assuming 1280x1024" >&2
