120e8e54b85ee5c87edb3d94fdcec6c992aec6fb
Adam Spiers <dotfiles@adamspiers.org>
2022-01-24 15:59:52 +0000

    fix display size detection and setting of Xft.dpi for HiDPI

--- lib/libdpy.sh
+++ lib/libdpy.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-typeset -a dpy_geometry
-dpy_geometry=( $(
-  xwininfo -root | \
-  awk '/-geometry/ { sub(/x/, " ", $2); sub(/\+.*/, "", $2); print $2 }'
-) )
-# this doesn't work under nomachine's NX client
-#dpy_geometry=($( xprop -root -notype 32i ' $0 $1\n' _NET_DESKTOP_GEOMETRY ))
+get_dpy_geometry () {
+    typeset -a dpy_geometry primary_geometry
+    dpy_geometry=(
+        $(
+            xwininfo -root | \
+                awk '/-geometry/ { sub(/x/, " ", $2); sub(/\+.*/, "", $2); print $2 }'
+        )
+    )
 
-# bash counts from 0, zsh counts from 1 - ugh!
-_array_index_test=(0 1 2)
-if [[ "${_array_index_test[1]}" == 1 ]]; then
-#     echo "shell array indices start at 0"
+    primary_geometry=(
+        $(
+            xrandr | awk '/primary/ { sub(/x/, " ", $4); sub(/\+.*/, "", $4); print $4 }'
+        )
+    )
+    # this doesn't work under nomachine's NX client
+    #dpy_geometry=($( xprop -root -notype 32i ' $0 $1\n' _NET_DESKTOP_GEOMETRY ))
+
+    if [ -n "$ZSH_VERSION" ]; then
+        # Make sure arrays start from 0 for this scope
+        setopt local_options ksh_arrays
+    fi
+
     dpy_width=${dpy_geometry[0]}
     dpy_height=${dpy_geometry[1]}
-elif [[ "${_array_index_test[1]}" == 0 ]]; then
-#     echo "shell array indices start at 1"
-    dpy_width=${dpy_geometry[1]}
-    dpy_height=${dpy_geometry[2]}
-fi
-unset _array_index_test
 
-if echo "$dpy_width$dpy_height" | egrep -q '[^0-9]'; then
-    echo "failed to determine desktop geometry; assuming 1280x1024" >&2
-    dpy_width=1280
-    dpy_height=1024
-fi
+    primary_width=${primary_geometry[0]}
+    primary_height=${primary_geometry[1]}
+
+    if echo "$dpy_width$dpy_height" | egrep -q '[^0-9]'; then
+        echo "failed to determine desktop geometry" >&2
+    fi
+}
+
+get_dpy_geometry
