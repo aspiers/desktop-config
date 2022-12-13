7be8699367bf4af3ce09dceaff60795a7eb57df2
Adam Spiers <dotfiles@adamspiers.org>
2022-12-13 17:04:41 +0000

    libdpy: extract secondary geometry

--- lib/libdpy.sh
+++ lib/libdpy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+extract_xrandr_geometry () {
+    xrandr | \
+        perl -lne "/$1/"' && / (\d+)x(\d+)\+(\d+)\+(\d+) / && print "width=$1\nheight=$2\nxoffset=$3\nyoffset=$4"' | \
+        sed "s/^/$2_/"
+}
+
 get_dpy_geometry () {
     typeset -a dpy_geometry primary_geometry
     dpi=(
@@ -16,11 +22,8 @@ get_dpy_geometry () {
         )
     )
 
-    primary_geometry=(
-        $(
-            xrandr | awk '/primary/ { sub(/x/, " ", $4); sub(/\+.*/, "", $4); print $4 }'
-        )
-    )
+    eval $( extract_xrandr_geometry ' connected primary ' primary )
+    eval $( extract_xrandr_geometry ' connected \d' secondary )
     # this doesn't work under nomachine's NX client
     #dpy_geometry=($( xprop -root -notype 32i ' $0 $1\n' _NET_DESKTOP_GEOMETRY ))
 
@@ -31,9 +34,6 @@ get_dpy_geometry () {
 
     dpy_width=${dpy_geometry[0]}
     dpy_height=${dpy_geometry[1]}
-
-    primary_width=${primary_geometry[0]}
-    primary_height=${primary_geometry[1]}
 
     if echo "$dpy_width$dpy_height" | egrep -q '[^0-9]'; then
         echo "failed to determine desktop geometry" >&2
